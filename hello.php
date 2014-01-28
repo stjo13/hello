754f12cd8f31b855def8b216fb83a6c4ae5e9413
Staffan Johansson <staffe.j.67@gmail.com>
2014-01-28 20:12:27 +0100

    Preparing to tag version 0.1

--- hello.php
+++ hello.php
@@ -2,8 +2,7 @@
 <head>
 	<title>Hello World </title>
 </head>
-<body>
-<h1>Hello</h1>
-	<?php echo "My name is Master Cock"; ?>
+<body>new version coming up</h1>
+	<?php echo "My name is Master Staff"; ?>
 </body>
 </html>
